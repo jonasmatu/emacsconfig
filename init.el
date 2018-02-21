390626f788cf17b77113718dd1978b332202b109
Noroskir <sentinaano@web.de>
2018-02-21 22:50:23 +0100

    M

--- init.el
+++ init.el
@@ -65,6 +65,12 @@
 (global-set-key (kbd "M-p") 'elpy-nav-backward-block)  ;;move bock up
 (global-set-key (kbd "M-,") 'pop-tag-mark) ;; go to last place where M-. was used (go-to-definition)
 
+;; Jupyter IPython
+;; -----------------------------------
+(setq ein:use-auto-complete t)
+(global-set-key (kbd "C-c C-n l") 'ein:notebooklist-login)
+(global-set-key (kbd "C-c C-n o") 'ein:notebooklist-open)
+
 ;; C++
 ;; --------------------------
 (setq c-default-style "linux"
