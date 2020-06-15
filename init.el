15aa20f35d5054691fc3bcb767dd0855ce106303
noroskir <sentinaano@web.de>
2020-06-15 13:44:55 +0200

    subatomic theme

--- init.el
+++ init.el
@@ -24,10 +24,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-safe-themes
+   (quote
+    ("b7133876a11eb2ded01b4b144b45d9e7457f80dd5900c332241881ab261c50f4" default)))
  '(package-selected-packages
    (quote
-    (flycheck-google-cpplint which-key vterm use-package try smartparens quelpa py-autopep8 parsec org-bullets multiple-cursors material-theme lsp-julia flycheck elpy doom-themes counsel better-defaults avy auctex atom-one-dark-theme))))
+    (subatomic-theme which-key use-package try smartparens py-autopep8 org-bullets multiple-cursors material-theme lsp-julia flycheck-julia elpy counsel company-auctex color-theme-sanityinc-tomorrow better-defaults avy atom-one-dark-theme)))
+ '(subatomic-more-visible-comment-delimiters t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
