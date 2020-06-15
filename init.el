7eafeab7d488c007f539bf884821b5bf92743008
noroskir <sentinaano@web.de>
2020-06-15 11:34:14 +0200

    latex

--- init.el
+++ init.el
@@ -17,11 +17,14 @@
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
  '(package-selected-packages
    (quote
     (flycheck-google-cpplint which-key vterm use-package try smartparens quelpa py-autopep8 parsec org-bullets multiple-cursors material-theme lsp-julia flycheck elpy doom-themes counsel better-defaults avy auctex atom-one-dark-theme))))
